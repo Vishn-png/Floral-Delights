5cd7e8d223feb833e8c8db4b924ee2eb47661d69
Vishn-png <20220909@mywhitecliffe.com>
2024-06-20 23:26:07 +1200

    created sign in form,updated nav links

--- cart.js
+++ cart.js
@@ -62,3 +62,6 @@ $(document).ready(function () {
   });
 });
 
+$("button").click(function(){
+  $("div").clearQueue();
+});
---
+++ signin.js
@@ -0,0 +1,28 @@
+function toggleSignIn() {
+  $('#signInModalUnique').modal('toggle');
+}
+
+// Handle form submission
+$('#signInForm').submit(function(event) {
+  event.preventDefault();
+  
+  // Perform validation and sign in logic here
+  var username = $('#username').val();
+  var password = $('#password').val();
+
+  // For demonstration purposes, simply log the inputs
+  console.log("Username: " + username);
+  console.log("Password: " + password);
+
+  // You can perform actual sign-in logic (e.g., AJAX request to server) here
+  // Replace this with your own authentication logic
+   // Clear the form fields
+   $('#username').val('');
+   $('#password').val('');
+
+   // Close the modal
+   $('#signInModalUnique').modal('hide');
+ });
+
+
+
