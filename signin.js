d5ecbe47c35009ea2c25fde6362a63dac5a71e16
Vishn-png <20220909@mywhitecliffe.com>
2024-06-27 09:51:10 +1200

    updated Cart and Sign-in pages

--- signin.js
+++ signin.js
@@ -1,30 +1,18 @@
-function toggleSignIn() {
-  $('#signInModalUnique').modal('toggle');
-}
+(document).ready(function() {
+$("#Form").submit(function(event) {
+      event.preventDefault();
+      
+      // Perform SignIn logic here
+      var username = $("#username").val();
+      var password = $("#password").val();
+      
+      // Validate the username and password
+  if (username === "example's" && password === "password123") {
+          alert("Sign In successful!");
+     } else {
+          alert("Invalid username or password. Please try again.");
+      }
+  });
+});
 
-// Handle form submission
-$('#signInForm').submit(function(event) {
-  event.preventDefault();
-  
-  // Perform validation and sign in logic here
-  var username = $('#username').val();
-  var password = $('#password').val();
-
-  // For demonstration purposes, simply log the inputs
-  console.log("Username: " + username);
-  console.log("Password: " + password);
-
-  // You can perform actual sign-in logic (e.g., AJAX request to server) here
-  // Replace this with your own authentication logic
-   // Clear the form fields
-   $('#username').val('');
-   $('#password').val('');
-
-   // Close the modal
- });
-
- function signIn() {
-  // Add your sign-in logic here
-  alert("Signed in successfully!");
-}
 
